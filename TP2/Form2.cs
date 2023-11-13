a9bdcf0a167c1baab906d3ecc61d891d54b77578
OmarThioune <144357060+OmarThioune@users.noreply.github.com>
2023-11-13 15:52:29 -0500

    Étape 6 numericUpDownNbAdulte textchanged.cs

--- TP2/Form2.cs
+++ TP2/Form2.cs
@@ -10,16 +10,76 @@ using System.Windows.Forms;
 
 namespace TP2
 {
-    public partial class FormReservation : Form
-    {
-        public FormReservation()
-        {
-            InitializeComponent();
-        }
+       public bool validationNom = false;
+       public bool validationCourriel = false;
+       public bool validationNbAdulte = false;
+       public bool validationNbEnfant = false;
+       public bool validationTerrain = false;
 
-        private void label4_Click(object sender, EventArgs e)
-        {
+       public FormReservation()
+       {
+           InitializeComponent();
+       
 
-        }
-    }
+       }
+
+       internal void Affichage(Camping camp)
+       {
+
+           if (camp.image == "Lac")
+           {
+               pictureBoxPaysage2.Image = Properties.Resources.secteurLac;
+               textBoxTerrain5.Visible = true;
+               textBoxTerrain6.Visible = true;
+               textBoxTerrain7.Visible = true;
+               textBoxTerrain8.Visible = true;
+               comboBoxChoixTerrain.Items.Add("T5");
+               comboBoxChoixTerrain.Items.Add("T6");
+               comboBoxChoixTerrain.Items.Add("T7");
+               comboBoxChoixTerrain.Items.Add("T8");
+
+           }
+           else
+           {
+               pictureBoxPaysage2.Image = Properties.Resources.secteurFleuve;
+           }
+
+           label_nomCamping.Text = camp.NomCamping;
+
+
+       }
+
+       private void label4_Click(object sender, EventArgs e)
+       {
+
+       }
+
+       private void buttonFaireReservation_Click(object sender, EventArgs e)
+       {
+        
+       }
+
+       private void numericUpDownAdulte_ValueChanged(object sender, EventArgs e)
+       {
+           if (numericUpDownAdulte.Value  > 8)
+           {
+               errorProviderNbAdulte.SetError(numericUpDownAdulte, "Ce nombre doit etre inférieure à 8");
+               validationNbAdulte = false;
+           }
+           else
+           {
+               errorProviderNbAdulte.Clear();
+               validationNbAdulte = true;
+              
+           }
+
+           if (validationNbAdulte == true && validationNbEnfant == true && 
+               validationCourriel == true && validationNom == true && validationTerrain == true
+               )
+           {
+               buttonFaireReservation.Enabled = true;
+           
+           }
+
+       }
 }
